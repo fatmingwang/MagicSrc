0b8c96b6ade4ad2ecb9f1be9643db82e53786529
Fatming <osimejp@yahoo.co.jp>
2023-05-27 16:15:17 +0800

    minor change

--- Src/MagicTower/GameLib/FileNameDefine.h
+++ Src/MagicTower/GameLib/FileNameDefine.h
@@ -1,26 +1,9 @@
-#pragma once
+#ifndef _MAGIC_TOWER_FILE_NAME_DEFINE_H_
+#define _MAGIC_TOWER_FILE_NAME_DEFINE_H_
 
 
-#define	MAX_PLAYER	14
+extern char*	BATTLE_ATTACK_MOVE_OBJECT_FILE_NAME;
+extern char*	MAIN_ROLE_SETUP_DATA;
+extern WCHAR*	UI_INFO_MPDILIST_FILE_NAME;
 
-#define	PLAYER_DATA						"Fish/Player/PlayerData"
-#define	PLAYERUI_MPDI					L"Fish/Image/PlayerUI/PlayerUI.mpdi"
-#define	PLAYER_NUMRIAL					L"Fish/Image/PlayerUI/PlayerUI.pi"
-
-
-
-//fatming
-#define	MONSTER_DATA					"Fish/MonsterData/FishData.xml"
-#define	FISH_PROBABILITY				"Fish/Probability/Probability.xml"
-#define	SCENE_CHANGE_SETUP_FILE			"Fish/Image/BG/BGSetup.xml"
-
-#define	BULLET_DATA						"Fish/Bullet/Bullet.xml"
-
-//first i64 is total in,second is total out rest is player's money
-//but if auto shoot is enable player money won't be assign
-//fatming end
-
-#define	COINSTACK_DATA					"Fish/Player/CoinStackSetting.xml"
-#define WINMONEYEFFECT_DATA				"Fish/Player/WinMoneyEffectConfig.xml"
-#define	CONTROLPANEL_VERSION_DATA		"Fish/ControlPanel/Version.xml"
-#define	CONTROLPANEL_TIMESTAMP_DATA		"Fish/ControlPanel/TimeStamp.xml"
+#endif
